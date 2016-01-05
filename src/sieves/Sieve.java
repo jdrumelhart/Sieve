28de597367232fad45ac612b1544f3666d5e9417
jdrumelhart <jdrumelhart@yahoo.com>
2016-01-05 11:33:12 -0800

    Expanded abstract class sieve

--- src/sieves/Sieve.java
+++ src/sieves/Sieve.java
@@ -2,4 +2,11 @@ package sieves;
 
 public abstract class Sieve {
 	
+	public abstract void primes();
+	
+	public abstract int getLastTime();
+	
+	public abstract int getTimesRun();
+	
+	public abstract int getLastMemory();
 }
