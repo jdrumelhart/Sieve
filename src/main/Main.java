f3bd6c504d843265e7351cf9675d6079ea3a9e6d
jdrumelhart <jdrumelhart1@gmail.com>
2016-03-08 10:44:57 -0800

    Compared the results of eratosthenes and sundaram

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		Eratosthenes e = new Eratosthenes();
 		ArrayList<Integer> emap = e.primes();
-		
+		Sundaram s = new Sundaram();
+		ArrayList<Integer> smap = s.primes();
+		System.out.println(emap.equals(smap));
 	}
 }
--- src/sieves/Eratosthenes.java
+++ src/sieves/Eratosthenes.java
@@ -1,7 +1,6 @@
 package sieves;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Eratosthenes implements Sieve {	
 	
